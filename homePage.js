772e04b6467ef6b97a447dfaf2bfaca469ce2bc2
willc <willcrain1@gmail.com>
2022-10-13 14:43:58 -0400

    Added parse notes for reddit json

--- homePage.js
+++ homePage.js
@@ -4,13 +4,10 @@
 //   response.json().then((data) => console.log(data))
 // );
 
-connect("172.2.177.112", "6969");
-
-function consoleLog(data) {
-  console.log(data);
-}
-
-sendGETRequest(
-  "consoleLog",
-  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=34.893372%2C-82.711852&radius=2500&type=restaurant&key=$google"
-);
+fetch("https://www.reddit.com/r/wendys.json").then(function(response) {
+  if(response.ok) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  }
+});
