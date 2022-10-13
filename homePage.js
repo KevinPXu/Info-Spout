04d56f5fadc63bc1191d650bfa4e2113cccf8e4e
doldol123 <toruig59@gmail.com>
2022-10-13 14:13:50 -0700

    add autocomplete

--- homePage.js
+++ homePage.js
@@ -25,9 +25,38 @@ var search = (response) => {
       console.table(response)
       return response.json();
     })
-
 }
 
+//homepage search autocomplete
+$( function() {
+  var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+  ];
+  $("#tags" ).autocomplete({
+    source: availableTags
+  });
+} );
 
 
 
