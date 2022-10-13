c13cb4c8053c084797527a80dba22c1236d1ce7f
willc <willcrain1@gmail.com>
2022-10-13 15:48:56 -0400

    Created function to condense reddit response data

--- contentPage.js
+++ contentPage.js
@@ -1,8 +1,22 @@
-function condenseNYTimesData(response) {
+function condenseRedditData(data) {
+    var articles = [];
+    for(const a of data.data.children) {
+        articles.push({
+            "author": a.data.author,
+            "content": a.data.selftext,
+            "media": a.data.url,
+            "upvotes": a.data.ups,
+            "url": "https://reddit.com" + a.data.permalink
+        });
+    }
+    return articles;
+}
+
+function condenseNYTimesData(data) {
     var articles = [];
     for(const a of data.response.docs) {
         articles.push({
-            "title": a.headline,
+            "title": a.headline.print_headline,
             "image": "https://nytimes.com/" + a.multimedia[0].url,
             "lead_text": a.lead_paragraph,
             "authors": a.byline.person,
