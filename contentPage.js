aaddd47e1c6fe22652354531384e730e283f75fe
KevinPXu <kevinpxu21@gmail.com>
2022-10-13 15:27:29 -0500

    added HTML containers or each row of our site

---
+++ contentPage.js
@@ -0,0 +1,9 @@
+async function test() {
+  let res = await fetch(
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=zkidiktBcpfsGjeON9HaDv0qxuDvKevq"
+  );
+  let data = await res.json();
+  console.log(data);
+}
+
+test();
